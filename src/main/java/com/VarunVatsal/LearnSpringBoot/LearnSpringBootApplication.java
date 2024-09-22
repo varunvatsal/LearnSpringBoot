f5da821582f04413d8059d500764e93b7a9d7dd2
varunvatsal <varunvatsal963@gmail.com>
2024-09-23 01:27:53 +0530

    add custom derived queries using @Query annotation

--- src/main/java/com/VarunVatsal/LearnSpringBoot/LearnSpringBootApplication.java
+++ src/main/java/com/VarunVatsal/LearnSpringBoot/LearnSpringBootApplication.java
@@ -10,6 +10,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class LearnSpringBootApplication {
@@ -35,8 +36,35 @@ public class LearnSpringBootApplication {
 //		});
 
 		//how to get the data
+//		Iterable<User> foundByIdUser =  userRepo.findAllById();
 		//Optional<User> returnVal = userRepo.findById(id) -> returns Optional container
 		//Iterable<User> returnVal =  userRepo.findAll() -> return the table
+		//find data based ont other columns(non-primary key columns), using custom finder methods or derived query methods
+		List<User> returnValOne = userRepo.findByFirst_name("varun");
+		returnValOne.forEach(ele -> {
+			System.out.println(ele);
+		});
+		List<User> returnValTwo = userRepo.findByEmail("varunvatsal963@gmail.com");
+		returnValTwo.forEach(ele -> {
+			System.out.println(ele);
+		});
+
+		Iterable<User> users = userRepo.findAll();
+		Iterator<User> iterator = users.iterator();
+//		while (iterator.hasNext()) {
+//			User user = iterator.next();
+//			System.out.println(user);
+//		}
+
+//		iterator.forEachRemaining(new Consumer<User>() {
+//			public void accept(User user) {
+//				System.out.println(user);
+//			}
+//		});
+		
+//		iterator.forEachRemaining(user -> {
+//			System.out.println(user);
+//		});
 
 
 		//updating
@@ -48,6 +76,11 @@ public class LearnSpringBootApplication {
 //		userRepo.save(user);
 //		System.out.println(user);
 
+
+		//how to delete
+//		userRepo.deleteAll(); // can pass an iterable of users
+//		userRepo.deleteById();
+//		userRepo.deleteAllById();
 
 
 
